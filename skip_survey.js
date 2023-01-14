5c0568c954406c258db7deea596273b3a5a2ca9b
kennex666 <kennexvn666@gmail.com>
2023-01-14 12:59:12 +0700

    Bỏ qua khảo sát - Ver 1.0

--- skip_survey.js
+++ skip_survey.js
@@ -1,3 +1,10 @@
+/*
+ - Thông tin tác giả: @Kennex666
+ - Viết ngày 14/01/2023
+ - Phiên bản 1.0
+ - Tên script: Đánh giá khảo sát nhanh
+ */
+
 var elems = document.getElementsByClassName("group_cauhoi");
 for (var i = 0; i < elems.length; i++) {
   var radioButtons = elems[i].getElementsByTagName("input");
