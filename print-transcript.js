76ecbb7d09df531e11fa16065ce0c5af315e6dff
Dương Thái Bảo <me@dtbao.io.vn>
2026-02-09 15:29:50 +0700

    Print Transcript V2.0

--- print-transcript.js
+++ print-transcript.js
@@ -1,1 +1,1 @@
-javascript:document.getElementsByClassName("DTFC_ScrollWrapper")[0].style.height = null;document.getElementsByClassName("dataTables_scrollBody")[0].style.maxHeight = null
+javascript:document.getElementById("viewBangDiem").querySelector("div").classList.toggle("tbl-scrollable", false)
